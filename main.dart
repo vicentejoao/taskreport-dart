beeedbb3659ee6cf54556e0961440d7a6283d208
João Vicente <289024696+vicentejoao@users.noreply.github.com>
2026-05-31 15:49:54 -0300

    feat: exibe todas as tarefas convertidas em objetos

--- main.dart
+++ main.dart
@@ -1,1 +1,8 @@
-void main() {}
+import 'dados_tarefas.dart';
+import 'tarefa.dart';
+
+void main() {
+  List<Tarefa> tarefas = dadosTarefas.map(Tarefa.fromMap).toList();
+  print('TAREFAS CONVERTIDAS\n');
+  tarefas.forEach(print);
+}
